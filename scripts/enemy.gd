995bc4549d8b56ac0a4fa7f549900e70e95742c2
NurRifai99 <nurrahmanrifai3@gmail.com>
2024-10-16 17:42:23 +0700

    combat system

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,14 @@ var speed = 35
 var playa = null
 var player_chase = false
 
+var health = 100
+var player_inattack_zone = false
+
+var can_take_damage = true
+
 func _physics_process(delta):
+	deal_with_damage()
+	
 	if player_chase :
 		position += (playa.position - position).normalized() * speed * delta
 		move_and_collide(Vector2(0,0)) 
@@ -20,9 +27,33 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	playa = body
 	player_chase = true
-
-
-
+	
 func _on_area_2d_body_exited(body) -> void:
 	player_chase = false
 	playa = null
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func deal_with_damage():
+	if player_inattack_zone and Global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("slime health : ",health)
+			if health <= 0 :
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,25 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var attack_ip =  false
+
 const speed = 70
 var current_dir = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
+	enemy_attack()
+	attack()
+	
+	if health <= 0 :
+		player_alive = false
+		health = 0
+		print("player has been killed")
+		self.queue_free()
 	
 func player_movement(_delta):
 	velocity = Vector2.ZERO  # Reset velocity each frame
@@ -41,26 +56,76 @@ func play_anim(movement):
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
 			
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:	
+				anim.play("side_idle")
 	
 	if dir == "up":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
-			anim.play("back_idle")
+			if attack_ip == false:	
+				anim.play("back_idle")
 	
 	if dir == "down":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
-			anim.play("front_idle")
+			if attack_ip == false:	
+				anim.play("front_idle")
+
+func player():
+	pass 
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true 
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
 		
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 10
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+	 
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true # Replace with function body.
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+		
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	Global.player_current_attack = false
+	attack_ip = false	 # Replace with function body.
